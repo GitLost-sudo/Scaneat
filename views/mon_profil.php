f62bbc398c8eedbf9029704a5f78c2c099940bcb
mathis <vanhee@et.esiea.fr>
2025-04-23 14:57:14 +0200

    création de la page profil

---
+++ views/mon_profil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Profil</title>
+    <link rel="stylesheet" href="./public/styles/main.css">
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <nav></nav>
+</body>
+</html>
