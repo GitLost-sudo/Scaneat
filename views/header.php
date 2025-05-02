e67d6448a401303159b5e68bbd251c83993a24c4
Eric LY <ely@et.esiea.fr>
2025-05-02 11:29:22 +0200

    Création du header, footer et de la page d'inscription + css

---
+++ views/header.php
@@ -0,0 +1,17 @@
+<style>
+    header{
+        background-color: orangered;
+
+    }
+    img{
+        
+        height: 5vh;
+        width: 25vh;
+    }
+</style>
+
+<header>
+    <div>
+    <img src="../public\img\SCAN'EAT_Ecriture (1).png" alt="Scan Eat">
+    </div>
+</header>
