835354d73a22a30f55031d6abee8100fafc7a03a
mathis <vanhee@et.esiea.fr>
2025-05-04 17:32:05 +0200

    ajout de la base du controller mon_profil

---
+++ controllers/mon_profil_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+// model
+require_once __DIR__."/../models/compte_model.php";
+require_once __DIR__."/../models/recette_model.php";
+
+// traitement
+
+
+// view
+require_once __DIR__."/../views/mon_profil_view.php";
