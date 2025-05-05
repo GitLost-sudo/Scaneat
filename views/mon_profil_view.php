a0575f8d541967f6a156db4b1fcd91058587f11a
mathis <vanhee@et.esiea.fr>
2025-05-05 10:30:07 +0200

    j'ai enlever le logo dans le header

--- views/mon_profil_view.php
+++ views/mon_profil_view.php
@@ -9,10 +9,7 @@
 <body>
     <header>
         <a href="../controllers/mon_profil_controller.php"><img class="header_icon" src="../public/icons/profil.png" alt="Mon Profil"></a>
-        <div id="header_container">
-            <img id="header_logo" src="../public/img/logo.png" alt="Logo de Scan'Eat">
-            <img id="header_ecriture" src="../public/img/ecriture.png" alt="Scan'Eat">
-        </div>
+        <img id="header_ecriture" src="../public/img/ecriture.png" alt="Scan'Eat">
         <a href="../controllers/accueil_controller.php"><img class="header_icon" src="../public/icons/home.png" alt="Accueil"></a>
     </header>
     <main>
