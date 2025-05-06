8b4ca8b934007cd1206a6216f543fa1ae2e3ef3a
Prince Dakouri <dakouri@et.esiea.fr>
2025-05-06 15:31:22 +0200

    recuperation mot de passe html

---
+++ views/recuperation.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mot de passe oublié</title>
+    <link href="../style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="page-container">
+        <header>
+            <div class="logo">
+                <img src="../img/ecriture.png">
+            </div>
+            <div class="home-icon">
+                <img src="../img/home.png">
+            </div>
+        </header>
+        <div class="TitreRecuperationMDP"> Email du compte que vous voulez récupérer :</div>
+        <form action="" method="POST">
+            <div class="BlocFormulaire">
+                <div class="champ"> 
+                    <input type="text" name="email">
+                </div>
+                <input type="submit" value="Envoyer" name="valider" class="ButtonCreation">
+
+            </div>
+        </form>
+        <footer>
+            <img src="../img/carotte2.png">
+            <img src="../img/fromage2.png">
+            <img src="../img/pomme2.png">
+        </footer>
+    </div>
+</body>
+
+</html>
