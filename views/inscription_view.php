6265134657809657f2c5c04715d014cca4866da5
Eric LY <ely@et.esiea.fr>
2025-05-07 15:49:46 +0200

    resolve conflicts merge

--- views/inscription_view.php
+++ views/inscription_view.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'inscription</title>
+    <link rel="stylesheet" href="./../public/styles/common.css">
+    <link rel="stylesheet" href="./../public/styles/inscription.css">
 </head>
-<link rel="stylesheet" href="public\styles\inscription.css">
 <body> 
 <?php include('header.php'); ?>
 <div class="inscription">
@@ -20,16 +21,24 @@
     <label for="email">Enter your email: </label>
     <input type="email" name="email" id="email" required />
   </div>
+  <form action="controllers/inscription_controller.php" method="POST" class="form-example">
   <div class="form-example">
-    <label for="password">Enter your password: </label>
-    <input type="password" name="password" id="password" required />
-  </div>
+      <label for="username">Nom: </label>
+      <input type="text" name="username" id="username" required />
+    </div>
   <div class="form-example">
-    <input type="submit" value="Create account" />
-  </div>
-</form>
-
-
-<?php include('footer.php'); ?>
+      <label for="email">Email: </label>
+      <input type="email" name="email" id="email" required />
+    </div>
+    <div class="form-example">
+      <label for="password">Mot de passe: </label>
+      <input type="password" name="password" id="password" required />
+    </div>
+    <div class="form-example">
+      <input type="submit" value="Create account" />
+    </div>
+  </form>
+</main>
+<nav></nav>
 </body>
 </html>
