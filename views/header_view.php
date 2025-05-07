1d0c802f03af744b75e41c1a548d55b69bbbe182
mathis <vanhee@et.esiea.fr>
2025-05-07 11:04:08 +0200

    creation de la view header

---
+++ views/header_view.php
@@ -0,0 +1,5 @@
+<header>
+    <a href="../controllers/mon_profil_controller.php"><img class="header_icon" src="../public/icons/profil.png" alt="Mon Profil"></a>
+    <img id="header_ecriture" src="../public/img/ecriture.png" alt="Scan'Eat">
+    <a href="../controllers/accueil_controller.php"><img class="header_icon" src="../public/icons/home.png" alt="Accueil"></a>
+</header>
