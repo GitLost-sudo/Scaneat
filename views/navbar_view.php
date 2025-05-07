4a914b8c2ba69a0bf97daab29d5b99ab0e0fbe09
Samy <belmihoub@et.esiea.fr>
2025-05-07 15:03:57 +0200

    nettoyage Dev

--- views/navbar_view.php
+++
@@ -1,5 +0,0 @@
-<nav>
-    <a href="../controllers/scanner_controller.php"><img class="nav_icon" src="../public/icons/barcode.png" alt="Scanner des produits"></a>
-    <a href="../controllers/recettes_controller.php"><img class="nav_icon" src="../public/icons/recette.png" alt="Mes Recettes"></a>
-    <a href="../controllers/frigo_controller.php"><img class="nav_icon" src="../public/icons/frigo.png" alt="Mon Frigo"></a>
-</nav>
