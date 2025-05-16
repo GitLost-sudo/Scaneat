b9bd579f72ba348b1e654a74d4d693e6b6bdf5cf
mathis <vanhee@et.esiea.fr>
2025-05-16 14:21:06 +0200

    ajout du controller liste_recette

---
+++ controllers/liste_recette_controller.php
@@ -0,0 +1,14 @@
+<?php
+
+// controllers
+$vegetarien = '';
+$vegan = '';
+$sans_gluten = '';
+$sans_lactose = '';
+$halal = '';
+
+// models
+require_once __DIR__.'/../models/recette_model.php';
+
+// views
+require_once __DIR__.'/../views/liste_recette_view.php';
