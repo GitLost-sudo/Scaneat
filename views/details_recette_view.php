afbb50b6303820f099150f46666b856b25a328f4
mathis <vanhee@et.esiea.fr>
2025-05-20 15:18:18 +0200

    création de la view détails de la recette

---
+++ views/details_recette_view.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Détails de la recette</title>
+    <link rel="stylesheet" href="../public/styles/common.css">
+    <link rel="stylesheet" href="../public/styles/details_recette.css">
+</head>
+<body>
+    <?php
+    require_once __DIR__.'/../views/header_org.php';
+    ?>
+    <main>
+        <form class="recette_details" action="../controllers/details_recette_controller.php" method="post">
+            <div class="recette_header">
+                <a href="../controllers/liste_recette_controller.php">
+                    <img class="fleche_retour" src="../public/icons/fleche_retour.png" alt="flèche de retour">
+                </a>
+                <h1><?= $recette['strMeal'] ?></h1>
+                <?php
+                //if ($recette['isfavorite']) {
+                    ?>
+                    <a href="../controllers/ajouter_favoris_controller.php?id=<?= $recette['idMeal'] ?>">
+                        <img class="favori" src="../public/icons/star_empty.png" alt="Ajouter aux favoris">
+                    </a>
+                    <?php
+                /*} else {
+                    ?>
+                    <a href="../controllers/retirer_favoris_controller.php?id=<?= $recette['idMeal'] ?>">
+                        <img class="favori" src="../public/icons/star_full.png" alt="Retirer des favoris">
+                    </a>
+                    <?php
+                }*/
+                ?>
+            </div>
+            <img src="<?= $recette['strMealThumb'] ?>" alt="image de la recette" class="recette_image">
+            <div class="ingredients">
+                <h2>Ingrédients</h2>
+                <ul>
+                    <?php
+                    for ($i = 1; $i <= 20; $i++) {
+                        $ingredient = isset($recette["strIngredient$i"]) ? trim($recette["strIngredient$i"]) : '';
+                        $measure = isset($recette["strMeasure$i"]) ? trim($recette["strMeasure$i"]) : '';
+                        if (!empty($ingredient)) {
+                            ?>
+                            <li><?= $measure ?> <?= $ingredient ?></li>
+                            <?php
+                        }
+                    }
+                    ?>
+                </ul>
+            </div>
+            <div class="instructions">
+                <h2>Instructions</h2>
+                <p><?= $recette['strInstructions'] ?></p>
+            </div>
+            <select name="proportion" id="proportion">
+                    <option value="">Choisir le nombre de personnes</option>
+                    <option value="1">1 personne</option>
+                    <option value="2">2 personnes</option>
+                    <option value="3">3 personnes</option>
+                    <option value="4">4 personnes</option>
+                    <option value="5">5 personnes</option>
+                </select>
+            <input type="submit" value="Terminé">
+        </form>
+    </main>
+    <?php
+    require_once __DIR__.'/../views/nav_bar.php';
+    ?>
+</body>
+</html>
--- views/liste_recette_view.php
+++ views/liste_recette_view.php
@@ -65,7 +65,7 @@
             foreach ($recettes as $recette) {
                 ?>
                 <div class="recette">
-                    <a href="#"><img class="image_recette" src="<?= $recette['strMealThumb'] ?>" alt="image de la recette"></a>
+                    <a href="../controllers/details_recette_controller.php?id=<?= $recette['idMeal'] ?>"><img class="image_recette" src="<?= $recette['strMealThumb'] ?>" alt="image de la recette"></a>
                     <?php
                     /*if ($recette['is_favorite']) {
                         ?>
