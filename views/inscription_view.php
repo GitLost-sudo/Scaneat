cf871b46e823484e0de86f937c732bbd74d470d9
Prince Dakouri <dakouri@et.esiea.fr>
2025-05-25 12:26:54 +0200

    renouvellement du html de l'inscription

--- views/inscription_view.php
+++ views/inscription_view.php
@@ -1,40 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page d'inscription</title>
-    <link rel="stylesheet" href="./../public/styles/common.css">
-    <link rel="stylesheet" href="./../public/styles/inscription.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page d'inscription</title>
+
+  <link rel="stylesheet" href="../style.css">
 </head>
-<body> 
-<?php
-require_once __DIR__.'/../views/header_org.php';
-?>
-<main>
-<div class="inscription">
-    Inscription
-</div>
-  <form action="inscription_controller.php" method="POST" class="form-example">
-  <div class="form-example">
-      <label for="username">Nom: </label>
-      <input type="text" name="username" id="username" required />
-    </div>
-  <div class="form-example">
-      <label for="email">Email: </label>
-      <input type="email" name="email" id="email" required />
-    </div>
-    <div class="form-example">
-      <label for="password">Mot de passe: </label>
-      <input type="password" name="password" id="password" required />
-    </div>
-    <div class="form-example">
-      <input type="submit" value="Create account" />
-    </div>
-  </form>
-</main>
-<?php
-require_once __DIR__.'/../views/nav_bar.php';
-?>
+
+<body>
+   <header>
+            <div class="logo">
+                <img src="../img/ecriture.png">
+            </div>
+            <div class="home-icon">
+                <a href="../controllers/authentification_controller.php">
+                    <img src="../img/home.png">
+                </a>
+            </div>
+        </header>
+        
+  <div class="page-container">
+
+    <div class="titreInscription"> INSCRIPTION</div>
+    <form action="inscription_controller.php" method="POST">
+
+      <div class="champ"> Nom : <br>
+        <input type="text" name="username" id="username">
+      </div>
+      <div class="champ"> Email :  <br>
+        <input type="email" name="email" id="email">
+      </div>
+      <div class="champ"> Mot de passe : <br>
+        <input type="password" name="password" id="password">
+      </div>
+      <input type="submit" value="Create" class="ButtonCreation" />
+
+    </form>
+
+    <footer>
+      <img src="../img/carotte2.png">
+      <img src="../img/fromage2.png">
+      <img src="../img/pomme2.png">
+    </footer>
+  </div>
+
 </body>
+
 </html>
