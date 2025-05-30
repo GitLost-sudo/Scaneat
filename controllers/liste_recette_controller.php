0559b4d967f7170a4df57f3a3ff750c8b9cac30d
mathis <vanhee@et.esiea.fr>
2025-05-30 09:28:52 +0200

    ajout du filtre du frigo

--- controllers/liste_recette_controller.php
+++ controllers/liste_recette_controller.php
@@ -15,8 +15,7 @@ $sans_gluten = '';
 $sans_lactose = '';
 $halal = '';
 
-$recettes = list_recette_by_filters($vegetarien, $vegan, $sans_gluten, $sans_lactose, $halal);
-
+$recettes = list_recette_by_filters($vegetarien, $vegan, $sans_gluten, $sans_lactose, $halal, $compte_id);
 
 // views
 require_once __DIR__.'/../views/liste_recette_view.php';
