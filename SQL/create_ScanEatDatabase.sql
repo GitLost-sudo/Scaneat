b42a721430d2a1fef581a619eb52e09b8db7f7a5
mathis <vanhee@et.esiea.fr>
2025-06-02 18:54:51 +0200

    ajout de la table favoris dans la base de données

--- SQL/create_ScanEatDatabase.sql
+++ SQL/create_ScanEatDatabase.sql
@@ -1,34 +1,28 @@
-DROP DATABASE IF EXISTS space;
+DROP DATABASE IF EXISTS ScanEat;
 CREATE DATABASE IF NOT EXISTS ScanEat DEFAULT CHARACTER SET = 'utf8mb4';
 USE ScanEat;
 
 CREATE TABLE IF NOT EXISTS `compte`(
-    `compte_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(255) UNIQUE NOT NULL,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `password` VARCHAR(255) NOT NULL
+  `compte_id` INT AUTO_INCREMENT PRIMARY KEY,
+  `username` VARCHAR(255) UNIQUE NOT NULL,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `password` VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS `frigo`;
 CREATE TABLE `frigo` (
- `id`INT AUTO_INCREMENT,
- `compte_id`INT NOT NULL,
- `categorie`VARCHAR(255),
- `quantite`INT,
- `date_peremption`DATE,
- PRIMARY KEY (`id`),
- FOREIGN KEY (`compte_id`) REFERENCES `compte`(`compte_id`)
+  `frigo_id`INT AUTO_INCREMENT,
+  `compte_id`INT NOT NULL,
+  `nom` VARCHAR(255) NOT NULL,
+  `categorie`VARCHAR(255),
+  `quantite`INT,
+  `date_peremption`DATE,
+  PRIMARY KEY (`frigo_id`),
+  FOREIGN KEY (`compte_id`) REFERENCES `compte`(`compte_id`)
 );
 
-CREATE TABLE `produit` (
-  `produit_id` INT NOT NULL,
-  `nom` VARCHAR(255) NOT NULL,
-  `image_url` VARCHAR(255)
-) 
-
 CREATE TABLE `favoris` (
-    `compte_id` INT NOT NULL,
-    `produit_id` INT NOT NULL,
-    FOREIGN KEY (`compte_id`) REFERENCES `compte`(`compte_id`),
-    FOREIGN KEY (`produit_id`) REFERENCES `produit`(`produit_id`)
+  `compte_id` INT NOT NULL,
+  `recette_id` INT NOT NULL,
+  FOREIGN KEY (`compte_id`) REFERENCES `compte`(`compte_id`)
 );
