39f6b529fc39c6d365d1e8a81546dc3a0981a691
mathis <vanhee@et.esiea.fr>
2025-06-03 15:20:59 +0200

    ajout de la fonction de suppression automatique des produits d'une recette

--- models/supprimer_produit_frigo_model.php
+++ models/supprimer_produit_frigo_model.php
@@ -7,3 +7,13 @@ function supprimer_produit_frigo($frigo_id) {
     $query = $db->prepare($sql);
     $query->execute([ ':frigo_id' => $frigo_id ]);
 }
+
+function supprimer_produit_frigo_by_nom($nom, $compte_id) {
+    global $db;
+    $sql = "DELETE FROM frigo WHERE nom = :nom AND compte_id = :compte_id";
+    $query = $db->prepare($sql);
+    $query->execute([
+        ':nom' => $nom,
+        ':compte_id' => $compte_id
+    ]);
+}
