392af454d233928b965c8ad46f4b6ddca0b22267
Eric LY <ely@et.esiea.fr>
2025-06-19 09:22:31 +0200

    unique retirer dans la bdd pour le username

--- SQL/create_ScanEatDatabase.sql
+++ SQL/create_ScanEatDatabase.sql
@@ -4,7 +4,7 @@ USE ScanEat;
 
 CREATE TABLE `compte`(
     `compte_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(255) UNIQUE NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL
 );
